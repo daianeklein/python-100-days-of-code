70cb2bb5849d21c5ef366687403045d4431fc497
daianeklein <daiane.klein22@gmail.com>
2022-07-13 17:37:30 -0300

    add new files

---
+++ 01_1-working-with-variables.py
@@ -0,0 +1,29 @@
+# print('Hello World!')
+
+# print('Day 01 - Python Print Function\nThe function is declared like this:')
+# print("print('what to print')")
+
+# #concat
+# print('Hello' + 'You')
+# print('Hello ' + 'You')
+# print('Hello' + ' You')
+# print('Hello' + ' ' + 'You')
+
+# #input
+# #input("What's your name? ")
+
+# print("Hello " + input("What's your name? " ))
+
+# # exercise 01
+# print(len(input('Tell me your name: ')))
+
+# # exercise 02
+# a = input("a: ")
+# b = input("b: ")
+
+# c = a
+# a = b
+# b = c
+
+# print("a: " + a)
+# print("b: " + b)
