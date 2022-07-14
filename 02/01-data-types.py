7d558a79cde5ac449e13a9492db5bda8b5f9b3ad
daianeklein <daiane.klein22@gmail.com>
2022-07-13 21:24:21 -0300

    add day 02

---
+++ 02/01-data-types.py
@@ -0,0 +1,25 @@
+#data types
+
+#strings
+print("Hello"[0]) #subscripting
+print('Hello'[-1])
+
+#Int
+print(123 + 1)
+print(123_345_789 + 10000)
+
+#Float
+#floating point number
+3.14159
+
+#data type
+num_char = len(input('What is your name?'))
+type(num_char)
+
+new_num_char = str(num_char)
+print("Your name has " + new_num_char + " characters.")
+
+a = float("100.5")
+print(type(a))
+
+print(70 + float("100.5"))
