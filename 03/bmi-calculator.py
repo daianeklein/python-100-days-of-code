d7acfdb954ac2fce5b1ff3a4bd20792097d548da
daianeklein <daiane.klein22@gmail.com>
2022-07-15 17:14:47 -0300

    add calculator exercise

---
+++ 03/bmi-calculator.py
@@ -0,0 +1,20 @@
+# Instructions
+# Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+# It should tell them the interpretation of their BMI based on the BMI value.
+
+# Under 18.5 they are underweight
+# Over 18.5 but below 25 they have a normal weight
+# Over 25 but below 30 they are slightly overweight
+# Over 30 but below 35 they are obese
+# Above 35 they are clinically obese.
+# https://cdn.fs.teachablecdn.com/qTOp8afxSkGfU5YGYf36
+
+# The BMI is calculated by dividing a person's weight (in kg) by the square of their height (in m):
+
+# https://cdn.fs.teachablecdn.com/jKHjnLrNQjqzdz3MTMyv
+
+# Warning you should round the result to the nearest whole number. The interpretation message needs 
+# to include the words in bold from the interpretations above. e.g. underweight, normal weight, 
+# overweight, obese, clinically obese.
+
