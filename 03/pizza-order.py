9b19ac25345c3b97e61b5404364b2eaf7bfc3649
daianeklein <daiane.klein22@gmail.com>
2022-07-19 22:25:38 -0300

    add exercise file

--- 03/pizza-order.py
+++ 03/pizza-order.py
@@ -15,5 +15,19 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
+price = 0
+if size == 'S':
+    price += 15
+    if add_pepperoni == 'Y':
+        price += 2
+elif size == 'M':
+    price += 20
+elif size == 'L':
+    price += 25
+    if add_pepperoni == 'Y':
+        price += 3
 
+if extra_cheese == 'Y':
+    price += 1
 
+print(price)
