8d4834b39c003ec45b7e7b70bfc64eb7180ad514
daianeklein <daiane.klein22@gmail.com>
2022-07-21 19:33:42 -0300

    exercise solution

--- 03/love-calculator.py
+++ 03/love-calculator.py
@@ -5,27 +5,43 @@
 # Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers 
 # to make a 2 digit number.
 
-# print("Welcome to the Love Calculator!")
-# name1 = input("What is your name? \n")
-# name2 = input("What is their name? \n")
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
 
-# nome1 = name1.lower()
-# name2 = name2.lower()
+names_combined = name1.lower() + name2.lower()
+
+count_list_true = []
+count_list_love = []
+
+for i in 'true':
+    count_list_true.append(names_combined.count(i))
+
+for j in 'love':
+    count_list_love.append(names_combined.count(j))
+
+true_count_words = sum(count_list_true)
+love_count_words = sum(count_list_love)
+
+print(true_count_words, love_count_words)
 
 
+# For Love Scores less than 10 or greater than 90, the message should be:
+# "Your score is **x**, you go together like coke and mentos."
 
-count_first_name = 0
-count_second_name = 0
-# for i in name1:
-#     if i in 'true love':
-#         count_first_name += 1
+# For Love Scores between 40 and 50, the message should be:
+# "Your score is **y**, you are alright together."
 
-# for j in name2:
-#     if j in 'true love':
-#         count_second_name +=1
+# Otherwise, the message will just be their score. e.g.:
+# "Your score is **z**."
 
-nomes = 'daianeJuliana'
-nomes = nomes.lower()
-t = nomes.count('j')
-print(t)
 
+score = str(true_count_words) + str(love_count_words)
+score = int(score)
+
+if score < 11 and score > 90:
+    print(f'Your Score is {score}, you go together like coke and mentos')
+elif score > 39 and score < 51:
+    print(f'Your score is {score}, you are alright together')
+else:
+    print(f'Your score is {score}')
