25a44bc2b662c781572717c4bf3ae8c15cdc41ed
daianeklein <daiane.klein22@gmail.com>
2022-07-21 19:02:50 -0300

    add love calculator

---
+++ 03/love-calculator.py
@@ -0,0 +1,31 @@
+# Instructions
+# You are going to write a program that tests the compatibility between two people.
+# To work out the love score between two people:
+# Take both people's names and check for the number of times the letters in the word TRUE occurs. 
+# Then check for the number of times the letters in the word LOVE occurs. Then combine these numbers 
+# to make a 2 digit number.
+
+# print("Welcome to the Love Calculator!")
+# name1 = input("What is your name? \n")
+# name2 = input("What is their name? \n")
+
+# nome1 = name1.lower()
+# name2 = name2.lower()
+
+
+
+count_first_name = 0
+count_second_name = 0
+# for i in name1:
+#     if i in 'true love':
+#         count_first_name += 1
+
+# for j in name2:
+#     if j in 'true love':
+#         count_second_name +=1
+
+nomes = 'daianeJuliana'
+nomes = nomes.lower()
+t = nomes.count('j')
+print(t)
+
