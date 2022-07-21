ab5877277b11ff30d575a98a63b6e5608ca9ad43
daianeklein <daiane.klein22@gmail.com>
2022-07-20 22:52:42 -0300

    add exercise

--- 03/pizza-order.py
+++ 03/pizza-order.py
@@ -16,18 +16,22 @@ add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 
 price = 0
+
 if size == 'S':
     price += 15
-    if add_pepperoni == 'Y':
-        price += 2
 elif size == 'M':
     price += 20
 elif size == 'L':
     price += 25
-    if add_pepperoni == 'Y':
+
+if add_pepperoni == 'Y':
+    if size == 'S':
+        price += 2
+    else:
         price += 3
 
 if extra_cheese == 'Y':
     price += 1
+
 
 print(price)
