494fb45a293434c9236b44634c2a50ec05baf8f1
daianeklein <daiane.klein22@gmail.com>
2022-07-22 12:07:59 -0300

    add files

---
+++ 03/07-project-day-treasure-island.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+decision_01 = input("Where you want to go? Left or Right? ").lower()
+
+
+if decision_01 != 'left':
+    print('You felt into a hole! Game Over')
+else:
+    decision_02 = input("You have reached a river! What you want to do? Swim or just wait? ").lower()
+    if decision_02 != 'wait':
+        print('You were attacked by a trout! Game Over')
+    else:
+        decision_03 = input("Now, choose a door: Red, Yellow or Blue? ").lower()
+        if decision_03 == 'red':
+            print('You were burned by a fire. Game Over!')
+        elif decision_03 == 'blue':
+            print('You were eaten by beasts. Game over!')
+        elif decision_03 == 'yellow':
+            print('You win! Congrats')
+        else:
+            print('You lost. Game Over!')
