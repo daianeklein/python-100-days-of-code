383ad7ef13f8740d19c359f9fcda66f5eb99aafd
daianeklein <daiane.klein22@gmail.com>
2022-07-25 11:36:42 -0300

    add exercise

---
+++ 04/04-banker-roulette.py
@@ -0,0 +1,17 @@
+# You are going to write a program which will select a random name from a list of names. 
+# The person selected will have to pay for everybody's food bill.
+
+# Important: You are not allowed to use the choice() function.
+
+# Line 8 splits the string names_string into individual names and puts them inside a 
+# List called names. For this to work, you must enter all the names as name followed by comma then space. e.g. name, name, name
+
+import random
+
+names_string = input("Give me evebody's names, separated by a comma: ")
+names_string = names_string.split(',')
+
+random_number = random.randint(0, len(names_string))
+random_name = names_string[random_number]
+
+print(f'{random_name} is going o buy the meal today!')
