434c9dc8569f34438f8b825e2a373240a29e98d3
daianeklein <daiane.klein22@gmail.com>
2022-08-24 19:09:21 -0300

    git ignore fixed

---
+++ 16/16-1/main.py
@@ -0,0 +1,17 @@
+#import turtle
+
+import my_module
+print(my_module.variable_name)
+
+
+#turtle object
+# import turtle
+# timmy = turtle.Turtle()
+
+from turtle import Turtle, Screen
+timmy = Turtle()
+
+print(timmy)
+
+timmy.mainloop()
+
---
+++ 16/16-1/my_module.py
@@ -0,0 +1,1 @@
+variable_name = 'Variable test'
