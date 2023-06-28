1864713c82778760b39cf873a8f1d9f26d3d3477
daianeklein <daiane.klein22@gmail.com>
2023-06-27 21:58:24 -0300

    add api module

---
+++ 33/main.py
@@ -0,0 +1,12 @@
+import requests
+
+response = requests.get('http://api.open-notify.org/iss-now.json')
+response.raise_for_status()
+
+data = response.json()
+print(data)
+
+longitude = data['iss_position']['longitude']
+latitude = data['iss_position']['latitude']
+iss_position = (longitude, latitude)
+print(iss_position)
