043eb6907d45766bce2e53aec545f4b1020fde88
daianeklein <daiane.klein22@gmail.com>
2023-06-30 19:16:23 -0300

    add finished project

--- 32/Birthday Wisher/main.py
+++ 32/Birthday Wisher/main.py
@@ -6,8 +6,13 @@ load_dotenv()
 my_email = 'dklein.dev@gmail.com'
 password = os.getenv('MY_PASSWORD_BIRTHDAY_WISHER')
 
-connection = smtplib.SMTP('smtp.gmail.com', 587)
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs='daiane.klein22@gmail.com', msg = 'Hello!')
-connection.close()
+with smtplib.SMTP('smtp.gmail.com', 587) as connection:
+    connection = smtplib.SMTP('smtp.gmail.com', 587)
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs='daiane.klein22@gmail.com',
+                        msg = 'Subject:Hello!\n\nThis is the body of my email')
+    
+
+#connection.close()
